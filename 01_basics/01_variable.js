e78c27ef79fc018907f389522bff66bf6c1acd3a
TIrth2305 <153996279+TIrth2305@users.noreply.github.com>
2023-12-31 07:24:11 +0000

    continer config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,30 @@
+const tirth=7799999
+/* keyword const is used to declare constent is javaScript 
+ -> it's not changeable  
+ */
+
+
+ let email="tirth45@my.com"
+ let  phno=7689456723
+ console.log(email)
+ console.log(phno) 
+ /* keyword let is used to declare variable in Js 
+ -> we must prefer to used let for declare variable */
+
+
+ var  city="jaipur"
+ console.log(city);
+ /* keyword var is used to declare variable in Js 
+ -> we not  prefer to used var for declare variable because of issue in block scope & functional scope */
+
+
+ myAge=17
+ console.log(myAge);
+ /*it is undefind variable with out any keyword
+ we also dont prefer to use this  */
+
+
+ let age;
+ console.log(age);
+ /* it's variable with out define value
+ it is call as undefinded variable */
